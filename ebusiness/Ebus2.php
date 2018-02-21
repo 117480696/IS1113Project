5173907778921e7f65b02522e9441091741591e9
Lauren Seery <117480696@umail.ucc.ie>
2018-02-21 13:52:48 +0000

    uploading changes to github

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -1,40 +1,33 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> Enter Details</title>
-
-
+        <title>Enter Details</title>
+        
         <!--jQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="ebus2_validator.js"></script>
     </head>
     <body>
-        <h4>Please enter your payment details.</h4>
+        <h4>Please enter your payment details</h4>
+        
+        
+            <form action="Ebus3.php" method="POST">
 
-            <br/>
+                    <label for="user_pin">PIN</label>
+                    
+                    <input type="password" id="user_pin" placeholder="Card PIN" maxlength="4">
 
-            <form method = "POST" action = "Ebus3.php">
-
-                <label for="user_pin">
-                     PIN
-                </label>
-
-                <input type="password" id="user_pin" placeholder="Card Pin" maxlength="4">
-
-                <button type="Submit" id="btnPurchase" disabled>
-                    Proceed with Purchase
-                </button>
-
+                <button type="submit" id="btnPurchase" disabled>Proceed with Purchase</button>
+              
             </form>
-
+            
             <br/>
-
-            <button onClick="validateDetails()"> Validate </button>
-
-        <script type="text/javascript" src="ebus2_validator.js"></script>
-
-
+            <button onClick="validateDetails()">Validate</button>
+            
+            <?php
+            // Set session variables
+            $_SESSION["total"] = $_POST["total"];
+            ?>
+        
     </body>
-
-
-
 </html>
